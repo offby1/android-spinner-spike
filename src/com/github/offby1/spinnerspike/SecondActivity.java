f49bbf8a8fef822688924ceaedd1f8db2f35ab51
Eric Hanchrow <eric.hanchrow@gmail.com>
2013-05-19 13:51:49 -0700

    Remove redundant overrides of onCreateOptionsMenu

--- src/com/github/offby1/spinnerspike/SecondActivity.java
+++ src/com/github/offby1/spinnerspike/SecondActivity.java
@@ -1,7 +1,6 @@
 package com.github.offby1.spinnerspike;
 
 import android.os.Bundle;
-import android.view.Menu;
 
 public class SecondActivity extends ActivityWithActionBar {
 
@@ -10,12 +9,4 @@ public class SecondActivity extends ActivityWithActionBar {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_second);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
